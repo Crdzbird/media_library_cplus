5e20fb4e24c4a661b623db43bf5f320bab9d9822
Crdzbird <luisalfonsocb83@gmail.com>
2024-04-10 17:03:07 +0200

    Unified Functions into one single

--- library.cpp
+++ library.cpp
@@ -4,8 +4,24 @@ extern "C" {
 #include <libavformat/avformat.h>
 }
 
+enum class MediaType {
+    Video,
+    Audio,
+    Unknown
+};
 
-double getVideoDuration(const char* filePath) {
+MediaType getMediaType(AVFormatContext* formatContext) {
+    for (unsigned int i = 0; i < formatContext->nb_streams; ++i) {
+        if (formatContext->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_VIDEO) {
+            return MediaType::Video;
+        } else if (formatContext->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
+            return MediaType::Audio;
+        }
+    }
+    return MediaType::Unknown;
+}
+
+double getMediaDuration(const char* filePath) {
     AVFormatContext* formatContext = nullptr;
     if (avformat_open_input(&formatContext, filePath, nullptr, nullptr) != 0) {
         avformat_close_input(&formatContext);
@@ -15,26 +31,16 @@ double getVideoDuration(const char* filePath) {
         avformat_close_input(&formatContext);
         return 0;
     }
-    double duration = static_cast<double>(formatContext->duration) / AV_TIME_BASE;
-    avformat_close_input(&formatContext);
-    return duration;
-}
-
-double getAudioDuration(const char* audioPath) {
-    AVFormatContext* formatContext = nullptr;
-    if (avformat_open_input(&formatContext, audioPath, nullptr, nullptr) != 0) {
-        avformat_close_input(&formatContext);
-        return 0;
-    }
-    if (avformat_find_stream_info(formatContext, nullptr) < 0) {
-        avformat_close_input(&formatContext);
-        return 0;
-    }
     double duration = 0;
-    for (unsigned int i = 0; i < formatContext->nb_streams; ++i) {
-        if (formatContext->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
-            duration = static_cast<double>(formatContext->duration) / AV_TIME_BASE;
-            break;
+    MediaType type = getMediaType(formatContext);
+    if (type == MediaType::Video) {
+        duration = static_cast<double>(formatContext->duration) / AV_TIME_BASE;
+    } else if (type == MediaType::Audio) {
+        for (unsigned int i = 0; i < formatContext->nb_streams; ++i) {
+            if (formatContext->streams[i]->codecpar->codec_type == AVMEDIA_TYPE_AUDIO) {
+                duration = static_cast<double>(formatContext->duration) / AV_TIME_BASE;
+                break;
+            }
         }
     }
     avformat_close_input(&formatContext);
--- library.h
+++ library.h
@@ -1,13 +1,11 @@
 #ifndef MEDIA_LIBRARY_LIBRARY_H
 #define MEDIA_LIBRARY_LIBRARY_H
 
-
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-double getVideoDuration(const char* filePath);
-double getAudioDuration(const char* audioPath);
+double getMediaDuration(const char* filePath);
 int isValidMediaFile(const char* filePath);
 
 #ifdef __cplusplus
